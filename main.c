83dc6a63fb3cc102290951362c0bb3f6e1023dc1
bim <iasmin.brebu@student.upt.ro>
2024-11-03 21:54:29 +0200

    i was suppose to only make future status, but made serial, oops

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 
 
 #define DEBUG
+#define INFECTION_RATE 1
+#define IMMUNE_DURATION 0
+#define INFECTED_DURATION 50
 
 typedef enum{
     INFECTED = 0,
@@ -24,12 +27,19 @@ typedef struct{
 }coords_t;
 
 typedef struct{
+    status_e status;
+    int immuneTime;
+    int infectionTime;
+    int infectionCounter;
+}status_t;
+
+typedef struct{
     int id;
     coords_t coords;
-    status_e status;
-    status_e futureStatus;
+    status_t status;
+    status_t futureStatus;
     cardinalDirections_e dir;
-    int movmentAmplitute;
+    int movementAmplitute;
 }person_t;
 
 typedef struct{
@@ -51,9 +61,18 @@ person_t *getPopulation(FILE *f, int size){
 
     for(int i = 0; i < size; i++){
         //id x y status dir amplitute
-        fscanf(f, "%d %d %d %d %d %d", &persons[i].id, &persons[i].coords.x, &persons[i].coords.y, (int*)&persons[i].status, (int*)&persons[i].dir, &persons[i].movmentAmplitute);
+        fscanf(f, "%d %d %d %d %d %d", &persons[i].id, &persons[i].coords.x, &persons[i].coords.y, (int*)&persons[i].status, (int*)&persons[i].dir, &persons[i].movementAmplitute);
+        
+        persons[i].status.immuneTime = 0;
+        persons[i].status.infectionTime = 0;
+        persons[i].status.infectionCounter = 0;
+        if(persons[i].status.status == INFECTED){
+            persons[i].status.infectionCounter = 1;
+            persons[i].status.infectionTime = INFECTED_DURATION;
+        }
+
         #ifdef DEBUG
-            fprintf(stderr, "%d %d %d %d %d %d\n", persons[i].id, persons[i].coords.x, persons[i].coords.y, (int)persons[i].status, (int)persons[i].dir, persons[i].movmentAmplitute);
+            fprintf(stderr, "%d %d %d %d %d %d\n", persons[i].id, persons[i].coords.x, persons[i].coords.y, (int)persons[i].status.status, (int)persons[i].dir, persons[i].movementAmplitute);
         #endif
     }
 
@@ -126,24 +145,127 @@ coords_t nextCoords(coords_t coords, int n, int m, cardinalDirections_e dir, int
 
 void updatePositions(person_t *persons, int start, int end, int n, int m, int round){
 
-    #ifdef DEBUG
-        fprintf(stderr, "Coords for t = %d\n", round);
-    #endif
     for(int i = start; i < end; i++){
-        persons[i].coords = nextCoords(persons[i].coords, n, m, persons[i].dir, persons[i].movmentAmplitute, &persons[i].dir);
-        #ifdef DEBUG
-            fprintf(stderr, "%d | %d %d\n", persons[i].id, persons[i].coords.x, persons[i].coords.y);
-        #endif
+        persons[i].coords = nextCoords(persons[i].coords, n, m, persons[i].dir, persons[i].movementAmplitute, &persons[i].dir);
     }
 
 }
 
-
-
-void makeContagionZone(paddedInt_t *contagionZone, int x, int y, int m){
-    contagionZone[y*m+x].data = 1;
+void makeContagionZone(paddedInt_t *contagionZone, coords_t coords, int m){
+    contagionZone[coords.y*m+coords.x].data = 1;
 }
 
+void updateContagionZone(person_t *persons, paddedInt_t *contagionZone, int start, int end, int m){
+    for(int i = start; i < end; i++){
+        if(persons[i].status.status == INFECTED){
+            makeContagionZone(contagionZone, persons[i].coords, m);
+        }
+    }
+}
+
+
+int coordIsContagious(paddedInt_t *contagionZone, coords_t coords, int m){
+    return contagionZone[coords.y*m + coords.x].data;
+}
+
+
+
+
+void updateFutureStatus(paddedInt_t *contagionZone, person_t *persons, int start, int end, int m){
+    for(int i = start; i < end; i++){
+        persons[i].futureStatus = persons[i].status;
+        if(persons[i].status.status == SUSCEPTIBLE) {
+            if(coordIsContagious(contagionZone, persons[i].coords, m) && rand()/(float)RAND_MAX <= INFECTION_RATE){
+                persons[i].futureStatus.status = INFECTED;
+                persons[i].futureStatus.infectionCounter++;
+                persons[i].futureStatus.infectionTime = INFECTED_DURATION;
+            }
+        }
+
+        if(persons[i].status.status == IMMUNE){
+            if(persons[i].status.immuneTime - 1 <= 0){
+                persons[i].futureStatus.status = SUSCEPTIBLE;
+            }
+            persons[i].futureStatus.immuneTime = persons[i].status.immuneTime > 0 ? persons[i].status.immuneTime - 1 : 0;
+        }
+
+        if(persons[i].status.status == INFECTED){
+            if(persons[i].status.infectionTime - 1 <= 0){
+                persons[i].futureStatus.status = IMMUNE;
+                persons[i].futureStatus.immuneTime = IMMUNE_DURATION;
+            }
+            persons[i].futureStatus.infectionTime = persons[i].status.infectionTime > 0 ? persons[i].status.infectionTime - 1 : 0;
+        }
+    }
+}
+
+void updateStatus(person_t *persons, int start, int end, int round){
+    #ifdef DEBUG
+        fprintf(stderr, "\n\nstatus for t = %d\n",round);
+    #endif
+    for(int i = start; i < end; i++){
+        #ifdef DEBUG
+            char *status;
+            char *nxtStatus;
+            if(persons[i].status.status == SUSCEPTIBLE){
+                status = "SUSCEPTIBLE";
+            }else if(persons[i].status.status == IMMUNE){
+                status = "IMMUNE";
+            }else{
+                status = "INFECTED";
+            }
+
+            if(persons[i].futureStatus.status == SUSCEPTIBLE){
+                nxtStatus = "SUSCEPTIBLE";
+            }else if(persons[i].futureStatus.status == IMMUNE){
+                nxtStatus = "IMMUNE";
+            }else{
+                nxtStatus = "INFECTED";
+            }
+            fprintf(stderr, "id: %d | (%d,%d) | %s -> %s | imn time: %d | inf time: %d | inf count: %d\n", persons[i].id, persons[i].coords.x, persons[i].coords.y, status, nxtStatus, persons[i].status.immuneTime, persons[i].status.infectionTime, persons[i].status.infectionCounter);
+        #endif
+        persons[i].status = persons[i].futureStatus;
+    }
+}
+
+void resetContagionZone(paddedInt_t *contagionZone, int start, int nmem){
+    memset(contagionZone + start, 0, nmem*sizeof(paddedInt_t));
+}
+
+void printVector(paddedInt_t *v, int n, int m){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            fprintf(stderr, "%d ", v[i*m+j].data);
+        }
+        fprintf(stderr, "\n");
+    }
+}
+
+void serialEpidemic(int n, int m, int size, person_t *persons, int rounds){
+    //1 = someone infected is there, 0 = nope
+    paddedInt_t *contagionZone = malloc(sizeof(paddedInt_t)*n*m);
+    if(contagionZone == NULL){
+        perror("Could not initiate contagious zones");
+        exit(-1);
+    }
+    memset(contagionZone, 0, sizeof(paddedInt_t)*n*m);
+
+    for(int i = 0; i < rounds; i++){
+        updatePositions(persons, 0, size, n, m, i);
+        updateContagionZone(persons, contagionZone, 0, size, m);
+        //in paralel a barrier here probs
+        updateFutureStatus(contagionZone, persons, 0, size, m);
+        updateStatus(persons, 0, size, i);
+        //fprintf(stderr, "%d\n", (int)persons[0].status.status);
+        #ifdef DEBUG
+            //printVector(contagionZone,n,m);
+        #endif
+        resetContagionZone(contagionZone, 0, m*n);
+    }
+
+
+    free(contagionZone);
+}
 
 
 int main(int argc, char **argv)
@@ -166,19 +288,12 @@ int main(int argc, char **argv)
     person_t *persons = getPopulation(f, size);
     fclose(f);
     
-    //1 = someone infected is there, 0 = nope
-    paddedInt_t *contagionZone = malloc(sizeof(paddedInt_t)*n*m);
-    if(contagionZone == NULL){
-        perror("Could not initiate contagious zones");
-        exit(-1);
-    }
-    memset(contagionZone, 0, sizeof(paddedInt_t)*n*m);
+    serialEpidemic(n, m, size, persons, 2);
 
 
 
 
 
-    free(contagionZone);
     free(persons);
     return 0;
 }
